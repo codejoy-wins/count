34139e5d73a624bc67af32d76a0231626b2f8939
Max <mjann@ucsc.edu>
2019-07-20 14:29:41 -0700

    react counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.double = this.double.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+  handleClick(){
+    console.log("clicker")
+    this.setState(prevState=>{
+      return{
+        count : prevState.count + 1
+      }
+    })
+    let msg = ""
+    document.getElementById("msgg").innerHTML = msg
+  }
+
+  double(){
+    console.log("doubling", this.state.count)
+    let msg
+    if(this.state.count===0){
+      console.log("error")
+      msg = "cannot double zero"
+      document.getElementById("msgg").innerHTML = msg
+    }
+    this.setState(prevState=>{
+      return{
+        count: prevState.count * 2
+      }
+    })
+  }
+
+  reset(){
+    
+    console.log("resetting", this.state.count)
+    this.setState({count:0})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        Count: {this.state.count} <hr></hr>
+        <button onClick={this.handleClick}>Increment</button>
+        <button onClick={this.double}>Double</button>
+        <button onClick={this.reset}>Reset</button>
+        <div id="msgg"></div>
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
